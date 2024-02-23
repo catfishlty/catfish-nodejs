3ea398e002bca87a080c5b4afbe8b673fa7c7098
catfishlty <catfish_lty@qq.com>
2024-02-23 11:17:52 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * index.js
+ * @author Catfish
+ * @email catfish_lty@qq.com
+ */
+
+class CatfishTest {
+    static print() {
+        console.log('Welcome to Catfish Home Cloud');
+    }
+}
+export default CatfishTest;
